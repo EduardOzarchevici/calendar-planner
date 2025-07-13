09b2b42839cd5e2fbb36bd2402a6310ea431dd75
EduardOzarchevici <iosuaozarchevici@gmail.com>
2025-07-13 22:55:36 +0300

    fix

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://admin:secretpassword@my_postgres_calendar:5432/calenderpalnnerdb')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'asdfhjfasiioh fasdfa sdfa sdf a1234h'
     JWT_ACCESS_TOKEN_EXPIRES = 3600
